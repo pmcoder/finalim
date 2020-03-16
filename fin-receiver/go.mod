8bbd9b8e4452f2752a32d2c379bcefbbbe68add4
Phillip Hagger <pmcoder7780@gmail.com>
2020-03-15 20:39:13 -0500

    adding files and commiting last work; receiving and posting to queue

--- fin-receiver/go.mod
+++ fin-receiver/go.mod
@@ -3,6 +3,7 @@ module github.com/pmcoder/finalim/fin-receiver
 go 1.13
 
 require (
+	github.com/nats-io/nats.go v1.9.1
 	github.com/pebbe/zmq4 v1.0.0
 	github.com/rs/zerolog v1.17.2
 )
--- fin-receiver/main.go
+++ fin-receiver/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	nats "github.com/nats-io/nats.go"
 	zmq "github.com/pebbe/zmq4"
 	"github.com/rs/zerolog/log"
 )
@@ -16,6 +18,11 @@ func main() {
 	}
 	defer listener.Close()
 
+	natsConn, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to NATS %s", err))
+	}
+
 	listener.Bind("tcp://*:5555")
 	log.Info().Msgf("Listening on port %d", 5555)
 	for {
@@ -29,9 +36,35 @@ func main() {
 		//for not just print it
 		log.Info().Msgf("Received msg: %s", msg)
 
+		err = enqueueMeessage(Message{}, natsConn)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to enqueue message")
+		}
+
 		listener.Send("Recv", 0)
 		/*if err != nil {
 			log.Error().Err(err)
 		}*/
 	}
+}
+
+func enqueueMeessage(message Message, nc *nats.Conn) error {
+	log.Info().Msg("Enqueueing message")
+	data, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	err = nc.Publish("messages-incoming", data)
+	if err != nil {
+		return err
+	}
+	log.Info().Msg("Enqueued message")
+	return nil
+
+}
+
+type Message struct {
+	Id          string
+	Content     string
+	Destination string
 }
